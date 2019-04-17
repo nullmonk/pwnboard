b31a64b6dd1439414b4a45649d5df62b5d3e8e78
micahjmartin <mjm5097@rit.edu>
2019-04-17 04:25:10 -0400

    Updating the setup

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -2,9 +2,20 @@
 
 # If there is a toplogy file, but not a board file, generate the baord
 # from the topology.json
-TOPO="config/topology.json"
+TOPO="topology.json"
 BOARD="board.json"
+
+# Check topo in the main dir first
 if [ -f "$TOPO" ] && [ ! -f "$BOARD" ]; then
     echo "Generating $BOARD from $TOPO"
     python3 "scripts/gen_config.py" "$TOPO" "$BOARD"
+    exit
+fi
+
+
+TOPO="config/topology.json"
+if [ -f "$TOPO" ] && [ ! -f "$BOARD" ]; then
+    echo "Generating $BOARD from $TOPO"
+    python3 "scripts/gen_config.py" "$TOPO" "$BOARD"
+    exit
 fi
